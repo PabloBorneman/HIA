92787b5d76b61132d09cc0c5885e595ac3be1bbb
Marcos Tarifa <marcos.tarifa@invoitrade.com>
2024-07-05 04:47:20 -0300

    login y registro personalizados añadidos

--- Frontend/src/app/components/header/header.component.ts
+++ Frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
