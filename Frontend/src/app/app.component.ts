8496d6697a182b9bff9b0a48924d1699b98493f5
Marcos Tarifa <marcos.tarifa@invoitrade.com>
2024-07-05 04:56:41 -0300

    creación de componentes del header y enrutamiento completos

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginModalComponent } from './components/modal/login-modal/login-modal.component';
 import { RegistroModalComponent } from './components/modal/registro-modal/registro-modal.component';
+import { HomeComponent } from './components/home/home.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LoginModalComponent, RegistroModalComponent],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    RouterLink,
+    HeaderComponent,
+    LoginModalComponent,
+    RegistroModalComponent,
+    HomeComponent,
+    CursosComponent,
+    NosotrosComponent,
+    ContactoComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-
 export class AppComponent {
   title = 'Frontend';
 }
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 export const routes: Routes = [
   {
@@ -8,7 +11,19 @@ export const routes: Routes = [
     component: LoginComponent,
   },
   {
-    path: 'home',
+    path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'cursos',
+    component: CursosComponent,
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent,
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent,
   },
 ];
