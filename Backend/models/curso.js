508b94b78c4e28c70b3a425b3525aae6b696a6d5
pavlo <pabloibarraborneman@gmail.com>
2024-07-12 00:48:53 -0300

    mas atributos en cursos y incriociones para profecionales

--- Backend/models/curso.js
+++ Backend/models/curso.js
@@ -5,6 +5,8 @@ const CursoSchema = new Schema({
     titulo: { type: String, required: true },
     descripcion: { type: String, required: true },
     linkvideo: { type: String, required: true },
+    linkvideoresumen: { type: String, required: false},
+    precio: { type: Number, required: true },
     instructor: { type: Schema.Types.ObjectId, ref: 'Usuario', required: true },
     miembros: [{ type: Schema.Types.ObjectId, ref: 'Usuario' }]
 });
--- Frontend/src/app/models/curso.ts
+++ Frontend/src/app/models/curso.ts
@@ -5,6 +5,8 @@ export class Curso {
   titulo!: string;
   descripcion!: string;
   linkvideo!: string;
+  linkvideoresumen?: string;  // Nuevo atributo opcional
+  precio?: number;  // Nuevo atributo opcional
   instructor!: Usuario | null;  // Cambiado a Usuario o null
   miembros!: Usuario[];  // Array de objetos Usuario
 
@@ -12,12 +14,16 @@ export class Curso {
     titulo: string = "",
     descripcion: string = "",
     linkvideo: string = "",
+    linkvideoresumen: string = "",  // Inicializar como cadena vacía
+    precio: number = 0,  // Inicializar como 0
     instructor: Usuario | null = null,
     miembros: Usuario[] = []
   ) {
     this.titulo = titulo;
     this.descripcion = descripcion;
     this.linkvideo = linkvideo;
+    this.linkvideoresumen = linkvideoresumen;
+    this.precio = precio;
     this.instructor = instructor;
     this.miembros = miembros;
   }
